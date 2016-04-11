6cba84584e30746dc25492080cf672d2a4626a21
Ch3LL <megan.wilhite@gmail.com>
2016-04-11 10:24:26 -0600

    add a bunch of stuff

--- salt-tests/script.sh
+++ salt-tests/script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-yum install git python-pip python-devel -y
+yum install git python-pip python-devel gcc gcc-c++ -y
 mkdir /home/tests/
 git clone https://github.com/saltstack/salt.git /home/tests/
 pip install -r /home/tests/requirements/dev_python27.txt
+pip install -e /home/tests/.
